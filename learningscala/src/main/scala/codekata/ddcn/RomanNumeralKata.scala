53f5fb48a5c5611963ee243dc71fd20b6544a01d
gaurav.gaur <gaurav.gaur@digital.hmrc.gov.uk>
2017-03-22 14:33:50 +0000

    paired up with Tom until now

--- learningscala/src/main/scala/codekata/ddcn/RomanNumeralKata.scala
+++ learningscala/src/main/scala/codekata/ddcn/RomanNumeralKata.scala
@@ -1,9 +1,17 @@
 package codekata.ddcn
 
+import IconNumbers._ 
+
 object RomanNumeralKata {
 
-	def arabicToRoman(arabic: Int): String = {
-		"Roman"
+	def arabicToRoman(arabic: Int, roman: String = ""): String = {
+		arabic match {
+			case lessThanFive if 5 % lessThanFive >= 3 => one * lessThanFive
+			case lessThanFive if 5 % lessThanFive <= 1 => one * lessThanFive + five
+			case fiveToTen if fiveToTen < 9 => five + one * (fiveToTen % 5)
+			case 9 => one + ten
+			case 10 => ten
+		}
 	}
 
 	def romanToArabic(roman: String) = {
@@ -13,3 +21,12 @@ object RomanNumeralKata {
 
 }
 
+object IconNumbers {
+
+	val one = "I"
+	val five = "V"
+	val ten = "X"
+	val fifty = "L"
+	val hundred = "C"
+	val fiveHundred = "D"
+}
