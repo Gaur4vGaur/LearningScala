d23a80d394ec9f7332fb91b01e6dfaf5f85bb509
Gaurav Gaur <vaibgaurav@gmail.com>
2017-04-19 21:27:59 +0100

    testcase to add scores of 7 bowls when third frame is spare

--- learningscala/src/test/scala/codekata/ddcn/BowlingGameKataSpec.scala
+++ learningscala/src/test/scala/codekata/ddcn/BowlingGameKataSpec.scala
@@ -9,12 +9,16 @@ class BowlingGameKataSpec extends FlatSpec with Matchers {
     calculateScore(Array(1,4)) shouldBe 5
   }
 
-  it should "add scores of 3 balls" in {
+  it should "add scores of 3 bowls" in {
     calculateScore(Array(1,4,4)) shouldBe 9
   }
 
-  it should "add scores of 4 balls" in {
+  it should "add scores of 4 bowls" in {
     calculateScore(Array(1,4,4,5)) shouldBe 14
+  }
+
+  it should "add scores of 7 bowls when third frame is spare" in {
+    calculateScore(Array(1,4,4,5,6,4)) shouldBe 29
   }
 
 }
