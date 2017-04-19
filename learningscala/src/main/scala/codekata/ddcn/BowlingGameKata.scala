9446490f332cd63d02059431da8583bd2b0c1f9b
Gaurav Gaur <vaibgaurav@gmail.com>
2017-04-19 22:19:37 +0100

    fixed  testcase for including 2 strikes and spares

--- learningscala/src/main/scala/codekata/ddcn/BowlingGameKata.scala
+++ learningscala/src/main/scala/codekata/ddcn/BowlingGameKata.scala
@@ -10,12 +10,21 @@ object BowlingGameKata {
     @tailrec
     def accumulate(scoresList: List[Int], isSpare:Boolean = false,
                    isStrike: Boolean = false, accumulator: Int = 0): Int = {
+
+      val calcBowl = (bowl: Int) => if(isSpare || isStrike) bowl * 2 else bowl
+      val calcSecondBowl = (secondBowl: Int) => if(isStrike) secondBowl * 2 else secondBowl
+
       scoresList match {
         case Nil => accumulator
+
         case bowl :: Nil => bowl + accumulator
-        case bowl :: tail if(bowl == 10) => accumulate(tail, isStrike = true, accumulator = (accumulator + bowl))
+
+        case bowl :: tail if(bowl == 10) =>
+          val sum = accumulator + calcBowl(bowl)
+          accumulate(tail, isStrike = true, accumulator = sum)
+
         case bowl :: secondBowl :: tail =>
-          val sum = accumulator + (if(isSpare || isStrike) bowl * 2 else bowl) + (if(isStrike) secondBowl * 2 else secondBowl)
+          val sum = accumulator + calcBowl(bowl) + calcSecondBowl(secondBowl)
           accumulate(tail, (bowl+secondBowl) == 10, accumulator = sum)
       }
     }
